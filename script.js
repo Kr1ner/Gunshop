5ea7f6336df9dd1e7e6ec032670eb77ebbe44334
Kr1ner <47250537+Kr1ner@users.noreply.github.com>
2022-02-03 23:36:40 +0400

    wfasf

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('../index.html',null,function(error,data){
+    fs.readFile('index.html',null,function(error,data){
         if(error){
             res.writeHead(404);
             res.writeFile('File Not Found!');
@@ -12,6 +12,19 @@ http.createServer(function (req, res) {
         res.end();
     })
   }).listen(8080);
-
-var jsondatabase = fs.readFile(database.json);
-var database = JSON.parse(jsondatabase);
+var database=JSON.parse(fs.readFileSync('database.json'));;
+var m = database.length;
+let data ={
+    id:m,
+    number:15+m,
+    username:"anzora",
+    name:"anzor mamaliadze"
+};
+database.push(data);
+fs.writeFile('database.json', JSON.stringify(database), (err) => {
+    if (err)
+        console.log(err);
+    else {
+        console.log("File written successfully\n");
+    }
+});
