66510287757bf5652514a053fdc4f0f89146a7a6
Irakli <47250537+Kr1ner@users.noreply.github.com>
2023-05-15 20:27:18 +0400

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    loader:rootLoader,
+    children:[
+      {
+        path: "aboutus",
+        element: <h1>despacito</h1>,
+        loader: teamLoader,
+      },
+    ]
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
